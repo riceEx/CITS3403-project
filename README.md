17ded67e950a3ccf4b4cb3a9ac4e000ec332453d
GooPoo <132414772+GooPoo@users.noreply.github.com>
2024-04-17 21:05:07 +0800

    Added Wordle word list and Table for the words

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Flask			3.0.2
 Flask-Login		0.6.3
 Flask-SQLAlchemy	3.1.1
 SQLAlchemy		2.0.29
+Flask-Session     0.8.0
 
 # Getting started
 run the following command
---
+++ insertData.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import Wordlewords  # Import your Wordlewords model
+
+# This python file purges the db to only have the bare bones of what we need in the database.
+# We should also add more 'table purges', so we don't have unnecessary data in the database.
+
+
+engine = create_engine('sqlite:///instance/messages.db')
+Session = sessionmaker(bind=engine)
+
+# Clear existing data from the Wordlewords table
+with Session() as session:
+    session.query(Wordlewords).delete()
+    session.commit()
+
+# Read the text file
+with open('data\\wordleValidWords.txt', 'r') as file:
+    words = [line.strip() for line in file]
+
+# Insert each word into the table
+with Session() as session:
+    for word in words:
+        new_word = Wordlewords(word=word)
+        session.add(new_word)
+    session.commit()
