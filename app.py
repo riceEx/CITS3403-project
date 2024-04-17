7585bd33e86b017bac678d0d8f7b46ca3294b3f0
riceEx <116779044+riceEx@users.noreply.github.com>
2024-04-17 13:43:30 +0800

    test
    juz a test push

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db.init_app(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+#.....
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
