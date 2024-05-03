dfa87370f8c3f6a2fb145cbc25f19e07f87c2f00
GooPoo <132414772+GooPoo@users.noreply.github.com>
2024-05-03 20:30:39 +0800

    Initial merging of Mark's Frontend
    Started to use Flask routes with Mark's html document.
    Cleaned up old webpage files,
    Initial boilerplate merged as well.

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from flask_session import Session
 from flask_socketio import SocketIO
 from sqlalchemy import func, event
 from werkzeug.security import generate_password_hash
-from database.models import db, User, Score, Wordlewords
+from database.models import db, User
 from werkzeug.exceptions import Unauthorized
 import sqlite3
-import utils
 import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 app.config['SESSION_TYPE'] = 'filesystem'
 Session(app)
 socketio = SocketIO(app)
@@ -21,13 +20,16 @@ db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,6 +52,7 @@ def register():
 
     return render_template('register.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -60,139 +63,20 @@ def login():
 
         if user and user.check_password(password):
             login_user(user)
-            return redirect(url_for('home'))
+            return redirect(url_for('welcomePage'))
         else:
             flash('Invalid username or password.', 'error')
 
     return render_template('login.html')
 
-@app.route('/home')
+
+@app.route('/welcomePage')
 @login_required
-def home():
+def welcomePage():
     if current_user.is_authenticated:
-        return render_template('home.html', user=current_user)
+        return render_template('welcomePage.html', user=current_user)
     else:
         return redirect(url_for('login'))
-
-@app.route('/forum')
-@login_required
-def forum():
-    if current_user.is_authenticated:
-        return render_template('forum.html', user=current_user)
-    else:
-        return redirect(url_for('login'))
-
-
-# Wordle Creation page
-@app.route('/wordleCreation')
-@login_required
-def wordleCreation():
-    if current_user.is_authenticated:
-        return render_template('wordleCreation.html', user=current_user)
-    else:
-        return redirect(url_for('login'))
-
-# On the Wordle Creation Page, Users can use this method to generate a random word for their wordle game.
-@app.route('/generate_word', methods=['GET'])
-@login_required
-def generate_word():
-    if not request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return jsonify({'error': 'Unauthorized'}), 403
-    random_word = Wordlewords.query.order_by(func.random()).first()
-    if random_word:
-        return jsonify({'word': random_word.word})
-    else:
-        return jsonify({'error': 'No words found'})
-
-# On the Wordle Creation Page, Users can use this method to validate their word is in the database of allowed words to create a wordle game.
-@app.route('/check_user_given_word', methods=['POST'])
-def check_user_given_word():
-    word_input = request.form['word']
-    word_exists = Wordlewords.query.filter_by(word=word_input).first() is not None
-    return jsonify({'exists': word_exists})
-
-
-
-# check if given word matches with the target word, return an array of [0, 1, 2]
-# @param word user input word
-# @param wordle_id which wordle_game to refer
-# return 0 if the ith letter is not in the target word
-# return 1 in cell i if the ith letter is in the word, but in a different place
-# return 2 in cell i if the ith letter is in the word and in the right place
-@app.route('/wordle/check_word', methods=['POST'])
-@login_required
-def checkWord():
-    word = request.form["word"]
-    wordle_id = request.form["wordle_id"]
-    # fetch target word, if not exist, return error
-    wordle = db.session.execute(db.select(Wordlewords).filter_by(id = int(wordle_id))).scalar_one_or_none()
-    if not wordle:
-        return jsonify({'error': 'No wordle found'})
-    return utils.checkWord(word, wordle.word)
-
-    
-# add_score, increment score to current user if existed, else create new score
-# @param user_id which user to be updated
-# @param score score to be incremented
-@app.route('/add_score', methods=['POST'])
-@login_required
-def add_score():
-    _user_id = request.form["user_id"]
-    _score = request.form["score"]
-
-    # fetch current score of user
-    score = db.session.execute(db.select(Score).filter_by(user_id=_user_id)).scalar_one_or_none()
-    # create score if not existed
-    if not score:    
-        score = Score(user_id=_user_id, score=int(_score))
-        db.session.add(score)
-    else:
-        score.score += int(_score)
-    db.session.commit()
-
-    flash('Score added!', 'success')
-    return redirect(url_for('index'))
-
-# event listener for score update
-# send top10 result to frontend when score has an update
-def after_update_listener(mapper, connection, target):
-    # state.get_history() can be used to get the history of attributes
-    # It returns a History object with three lists: added, unchanged, and deleted
-    state = db.inspect(target)
-    if state.attrs.score.history.has_changes():
-        scores = db.session.query(Score.user_id.label('user_id'), Score.score.label('score'), User.username.label('username')).join(User, Score.user_id == User.id).order_by(Score.score.desc()).paginate(page=1, per_page=10, error_out=False)
-        
-        result = [{'user_id': score.user_id, 'score': score.score, 'user_name': score.username} for score in scores.items]
-        # Emit the top 10 scores
-        socketio.emit('score_update', result)
-
-# Attach the listener to the Score class, listening to 'after_update'
-event.listen(Score, 'after_update', after_update_listener)
-
-# get_scores, get scores of all users
-# @param page_no page number
-# @param page_size page size
-# @param order: asc or desc
-@app.route('/get_scores', methods=['GET'])
-@login_required
-def get_scores():
-    if not request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        return jsonify({'error': 'Unauthorized'}), 403
-    page_no = request.args.get("page_no", 1)
-    page_size = request.args.get("page_size", 10)
-    order = request.args.get("order", 'desc')   
-    order_method = getattr(Score.score, order)
-
-    scores = db.session.query(Score.user_id.label('user_id'), Score.score.label('score'), User.username.label('username')).join(User, Score.user_id == User.id).order_by(order_method()).paginate(page=int(page_no), per_page=int(page_size), error_out=False)
-
-    # transform score models to list of dicts
-    # result = [score.to_dict() for score in scores.items]
-    result = [{'user_id': score.user_id, 'score': score.score, 'user_name': score.username} for score in scores.items]
-
-    flash('Score fetched!', 'success')
-    # return result as JSON
-    return jsonify({"result": result, "total": scores.total})
-
 
 
 @app.route('/logout')
